385d4ea358d68a129dec011e1e9039e4451f69c4
Victor Ulloa <alejandroekas@gmail.com>
2024-07-27 20:50:33 -0400

    project created

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Victor Ulloa on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
